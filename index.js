0ad4c6e0102b50b0f079a690922ec9bcc58db8ea
Jaakko Pääkkönen <pkjaa@digitalentshelsinki.internal>
2022-08-25 13:39:44 +0300

    Test 3

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post('/api/position', (request, response) => {
     response.json(JSON.stringify({bar: "foo"}))
   })
   
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
